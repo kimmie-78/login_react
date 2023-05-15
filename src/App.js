a3a322ac31b88722b56e10afb295766f1173ceef
kimmie-78 <101290243+kimmie-78@users.noreply.github.com>
2023-05-15 02:46:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [formData, setFormData] = useState({ Username: '', Useremail: '',Password:'' });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('formData', JSON.stringify(formData));
+    console.log(formData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+        <main id="main-holder">
+        <div className="header-container">
+          <h1 id="signin-header">SignIn</h1>
+
+          <div className="image-container">
+        <img
+            src="https://th.bing.com/th/id/R.7eafaa088b425eb078a54ffed1fe058d?rik=XjVXyFKRbAojxA&pid=ImgRaw&r=0"
+            alt="Metro Bus App"
+            style={{ float: 'right', width: '450px', height: '500px' }}
+          />
+      </div>
+      </div>
+          
+          <form id="Sign In-form" onSubmit={handleSubmit}>
+          <div className="input-group"> 
+          
+            <input
+              type="text"
+              name="Username"
+              id="username-field"
+              value = {formData.Username}
+              onChange={handleInputChange}
+              className="Sign In-form-field"
+              placeholder="Username"
+            />
+            </div>
+            <div className="input-group"> 
+           
+            <input
+              type="text"
+              name="Useremail"
+              id="useremail-field"
+              value={formData.Useremail}
+              onChange={handleInputChange}
+              className="Sign In-form-field"
+              placeholder="Email"
+            />
+            </div>
+            <div className="input-group"> 
+            <input
+              type="password"
+              name="Password"
+              id="password-field"
+              value={formData.password} 
+              onChange={handleInputChange}
+              className="Sign In-form-field"
+              placeholder="Password"
+            />
+            </div>
+            <div className="input-group"> 
+            <button
+              type="submit"
+              id="signin-form-submit">
+                SignIn
+            </button>
+            
+            </div>
+          </form>
+        </main>
+      
+   
     </div>
   );
-}
 
+  }
 export default App;
